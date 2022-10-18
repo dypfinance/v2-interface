effdcfc2f992f0e14b890ba9e8c75cead64cb058
LorenaL579 <lorena.lici579@gmail.com>
2022-10-18 10:57:02 +0200

    ui fixes for carousel

--- src/Screens/Home/MainHero/MainHero.js
+++ src/Screens/Home/MainHero/MainHero.js
@@ -5,7 +5,7 @@ import CometBg from "../../../components/CometBg/CometBg";
 import Slider from "react-slick";
 
 const MainHero = () => {
-  const [showDesc, setShowDesc] = useState("");
+  
 
   const settings = {
     dots: false,
@@ -18,7 +18,7 @@ const MainHero = () => {
     arrows: false,
   };
 
-  const slickref = useRef()
+  const slickref = useRef();
 
   return (
     <div className="container-fluid p-0" style={{ overflow: "hidden" }}>
@@ -42,72 +42,72 @@ const MainHero = () => {
                   Explore
                 </button>
               </div>
-              <div className="sliderwrapper  d-flex flex-column mt-3 ml-lg-4 pl-0">
+              <div className="sliderwrapper  d-flex flex-column mt-3 pl-0">
                 <Slider {...settings} ref={slickref}>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>Earn</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       Start earning high value rewards by depositing assets on
                       our immersive Dypius products and solutions
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>Governance</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       DYP holders can vote on whether to add more pools, burn
                       tokens, allocate DYP toward grants, strategic
                       partnerships, and different initiatives.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>DYP Tools</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       An advanced analytics data tools that provides easier
                       navigation to decentralized networks, new trends,
                       protection, support for new projects and much more.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>Bridge</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       Powerful dApp that allows to swap tokens between Ethereum
                       to Binance, Avalanche and many more to come. Instant and
                       secure transactions.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>Launchpad</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       Support new projects to raise capital in a decentralized
                       environment by bringing token offerings to the market in
                       the highest secure way.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>CAWS NFT</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       A limited Cat and Watches Society NFT collection of 10,000
                       randomly generated, hand-drawn utility NFTs developed by
                       Dypius.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>CAWS NFT</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       A limited Cat and Watches Society NFT collection of 10,000
                       randomly generated, hand-drawn utility NFTs developed by
                       Dypius. These unique NFTs consist of over 235 different
@@ -115,11 +115,11 @@ const MainHero = () => {
                       various rarities.
                     </p>
                   </div>
-                  <div className="d-flex flex-column gap-0">
+                  <div className="d-flex flex-column gap-0 rowwrapper">
                     <p>
                       <b>Metaverse</b>
                     </p>
-                    <p>
+                    <p style={{fontSize: 13}}>
                       An innovative metaverse platform that redefines the gaming
                       industry and digital currency services. Platform utilizes
                       cutting-edge blockchain technology, giving an unique
@@ -132,35 +132,28 @@ const MainHero = () => {
             <div className="col-12 col-lg-5 d-flex justify-content-center flex-column text-white ml-4">
               <DypSystem
                 onShowFirstDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(1)
-
+                  slickref.current.innerSlider.slickGoTo(1);
                 }}
                 onShowSecondDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(2)
-
+                  slickref.current.innerSlider.slickGoTo(2);
                 }}
                 onShowThirdDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(3)
+                  slickref.current.innerSlider.slickGoTo(3);
                 }}
                 onShowFourthDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(4)
-
+                  slickref.current.innerSlider.slickGoTo(4);
                 }}
                 onShowFifthDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(5)
-
+                  slickref.current.innerSlider.slickGoTo(5);
                 }}
                 onShowSixthDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(6)
-
+                  slickref.current.innerSlider.slickGoTo(6);
                 }}
                 onShowSeventhDesc={() => {
-                  slickref.current.innerSlider.slickGoTo(1)
-
+                  slickref.current.innerSlider.slickGoTo(1);
                 }}
                 onRemoveDesc={() => {
-                  slickref.current.innerSlider.slickNext()
-
+                  slickref.current.innerSlider.slickNext();
                 }}
               />
             </div>
