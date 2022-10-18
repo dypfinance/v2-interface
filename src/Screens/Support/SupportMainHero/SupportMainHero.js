25d8b01f58a6cfddee1e8a45c9e9bc850c8aff45
LorenaL579 <lorena.lici579@gmail.com>
2022-10-18 17:19:03 +0200

    ui fixes for support

--- src/Screens/Support/SupportMainHero/SupportMainHero.js
+++ src/Screens/Support/SupportMainHero/SupportMainHero.js
@@ -13,7 +13,7 @@ const SupportMainHero = () => {
   return (
     <div className="container-fluid d-flex flex-column justify-content-end support-main-wrapper">
         <div className="container-lg d-flex justify-content-center align-items-end p-5">
-        <div className="row justify-content-between w-100">
+        <div className="row justify-content-between w-100 align-items-center" style={{minHeight: 200}}>
         <div className="col-12 col-lg-5 col-md-5 mt-5 mt-lg-0 position-relative">
           <img src={sphere} alt="" className="support-sphere d-none d-lg-block" />
           <Title top="Dypius" bottom="Support" align="d-flex flex-row gap-2" />
