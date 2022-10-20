7284ad2c00fb3cfa302face1cf1e25e3d8af06f3
LorenaL579 <lorena.lici579@gmail.com>
2022-10-20 11:38:08 +0200

    fixed animation for press release

--- src/components/PressRelease/PressRelease.js
+++ src/components/PressRelease/PressRelease.js
@@ -16,145 +16,268 @@ const PressRelease = () => {
           align="d-flex gap-3 mb-5 justify-content-center"
         />
       </div>
-      <div className="position-relative">
+      <div className="position-relative overflow-hidden">
         <div className="leftbg"></div>
-      <table className="presstable">
-        <tbody className="pressbody">
-          <tr>
-            <td className="radius1 presscell"></td>
-            <td className="presscell radius2">
-              <img src={require("./news/coinmarketcap.png")} alt="" />
-            </td>
-            <td className="radius1 presscell"></td>
-            <td className="radius1 presscell">
-              <img src={require("./news/bitinsider.png")} alt="" />
-            </td>
-            <td className="presscell radius4"></td>
-            <td className="radius4 presscell"></td>
+        <table className="presstable">
+          <tbody className="pressbody">
+            <tr>
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius2">
+                <img src={require("./news/coinmarketcap.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+              <td className="radius1 presscell">
+                <img src={require("./news/bitinsider.png")} alt="" />
+              </td>
+              <td className="presscell radius4"></td>
+              <td className="radius4 presscell"></td>
 
-            <td className="radius2 presscell">
-              <img src={require("./news/medium.png")} alt="" />
-            </td>
+              <td className="radius2 presscell">
+                <img src={require("./news/medium.png")} alt="" />
+              </td>
 
-            <td className="presscell radius1"></td>
-            <td className="presscell radius5">
-              <img src={require("./news/bitcoincom.png")} alt="" />
-            </td>
-            <td className="radius6 presscell"></td>
-            <td className="radius5 presscell">
-              <img src={require("./news/gateio.png")} alt="" />
-            </td>
-            <td className="presscell radius2"></td>
-            <td className="radius1 presscell"></td>
-            <td className="radius6 presscell">
-              <img src={require("./news/bitcoinist.png")} alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td className="radius10 presscell"></td>
-            <td className="presscell radius5">
-              <img src={require("./news/yahoo.png")} alt="" />
-            </td>
-            <td className="presscell radius10"></td>
-            <td className="presscell radius2">
-              <img src={require("./news/marketwatch.png")} alt="" />
-            </td>
-            <td className="radius4 presscell"></td>
-            <td className="presscell radius10">
-              <img src={require("./news/apnews.png")} alt="" />
-            </td>
-            <td className="presscell radius1"></td>
-            <td className="presscell radius7">
-              <img src={require("./news/cryptofold.png")} alt="" />
-            </td>
-            <td className="presscell radius10">
-              <img src={require("./news/newsbtc.png")} alt="" />
-            </td>
-            <td className="presscell radius1"></td>
-            <td className="presscell radius8"></td>
-            <td className="presscell radius1"></td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius5">
+                <img src={require("./news/bitcoincom.png")} alt="" />
+              </td>
+              <td className="radius6 presscell"></td>
+              <td className="radius5 presscell">
+                <img src={require("./news/gateio.png")} alt="" />
+              </td>
+              <td className="presscell radius2"></td>
+              <td className="radius1 presscell"></td>
+              <td className="radius6 presscell">
+                <img src={require("./news/bitcoinist.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius2">
+                <img src={require("./news/coinmarketcap.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+              <td className="radius1 presscell">
+                <img src={require("./news/bitinsider.png")} alt="" />
+              </td>
+              <td className="presscell radius4"></td>
+              <td className="radius4 presscell"></td>
 
-            <td className="presscell radius7">
-              <img src={require("./news/hackernoon.png")} alt="" />
-            </td>
-            <td className="presscell radius1"></td>
-          </tr>
-          <tr>
-            <td className="presscell radius1">
-              <img src={require("./news/blokonomi.png")} alt="" />
-            </td>
-            <td className="radius10 presscell"></td>
-            <td className="presscell radius1">
-              <img src={require("./news/techbullion.png")} alt="" />
-            </td>
+              <td className="radius2 presscell">
+                <img src={require("./news/medium.png")} alt="" />
+              </td>
 
-            <td className="presscell radius6"></td>
-            <td className="radius1 presscell"></td>
-            <td className="presscell radius4">
-              <img src={require("./news/utoday.png")} alt="" />
-            </td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius5">
+                <img src={require("./news/bitcoincom.png")} alt="" />
+              </td>
+              <td className="radius6 presscell"></td>
+              <td className="radius5 presscell">
+                <img src={require("./news/gateio.png")} alt="" />
+              </td>
+              <td className="presscell radius2"></td>
+              <td className="radius1 presscell"></td>
+              <td className="radius6 presscell">
+                <img src={require("./news/bitcoinist.png")} alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className="radius10 presscell"></td>
+              <td className="presscell radius5">
+                <img src={require("./news/yahoo.png")} alt="" />
+              </td>
+              <td className="presscell radius10"></td>
+              <td className="presscell radius2">
+                <img src={require("./news/marketwatch.png")} alt="" />
+              </td>
+              <td className="radius4 presscell"></td>
+              <td className="presscell radius10">
+                <img src={require("./news/apnews.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/cryptofold.png")} alt="" />
+              </td>
+              <td className="presscell radius10">
+                <img src={require("./news/newsbtc.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius8"></td>
+              <td className="presscell radius1"></td>
 
-            <td className="presscell radius7"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/hackernoon.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+              <td className="radius10 presscell"></td>
+              <td className="presscell radius5">
+                <img src={require("./news/yahoo.png")} alt="" />
+              </td>
+              <td className="presscell radius10"></td>
+              <td className="presscell radius2">
+                <img src={require("./news/marketwatch.png")} alt="" />
+              </td>
+              <td className="radius4 presscell"></td>
+              <td className="presscell radius10">
+                <img src={require("./news/apnews.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/cryptofold.png")} alt="" />
+              </td>
+              <td className="presscell radius10">
+                <img src={require("./news/newsbtc.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+              <td className="presscell radius8"></td>
+              <td className="presscell radius1"></td>
 
-            <td className="presscell radius1">
-              <img src={require("./news/cryptopress.png")} alt="" />
-            </td>
-            <td className="radius7 presscell"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/hackernoon.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+            </tr>
+            <tr>
+              <td className="presscell radius1">
+                <img src={require("./news/blokonomi.png")} alt="" />
+              </td>
+              <td className="radius10 presscell"></td>
+              <td className="presscell radius1">
+                <img src={require("./news/techbullion.png")} alt="" />
+              </td>
 
-            <td className="presscell radius10">
-              <img src={require("./news/coinspeaker.png")} alt="" />
-            </td>
-            <td className="presscell radius1">
-              <img src={require("./news/cryptocompare.png")} alt="" />
-            </td>
-            <td className="presscell radius7">
-              <img src={require("./news/cointelegraph.png")} alt="" />
-            </td>
-            <td className="radius1 presscell"></td>
+              <td className="presscell radius6"></td>
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius4">
+                <img src={require("./news/utoday.png")} alt="" />
+              </td>
 
-            <td className="presscell radius7">
-              <img src={require("./news/mycointainer.png")} alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td className="presscell radius7">
-              <img src={require("./news/uptrennd.png")} alt="" />
-            </td>
-            <td className="radius1 presscell"></td>
+              <td className="presscell radius7"></td>
 
-            <td className="presscell radius6">
-              <img src={require("./news/coindex.png")} alt="" />
-            </td>
+              <td className="presscell radius1">
+                <img src={require("./news/cryptopress.png")} alt="" />
+              </td>
+              <td className="radius7 presscell"></td>
 
-            <td className="presscell radius1"></td>
-            <td className="presscell radius9">
-              <img src={require("./news/coindoo.png")} alt="" />
-            </td>
+              <td className="presscell radius10">
+                <img src={require("./news/coinspeaker.png")} alt="" />
+              </td>
+              <td className="presscell radius1">
+                <img src={require("./news/cryptocompare.png")} alt="" />
+              </td>
+              <td className="presscell radius7">
+                <img src={require("./news/cointelegraph.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
 
-            <td className="radius1 presscell"></td>
-            <td className="presscell radius8">
-              <img src={require("./news/coinmanager.png")} alt="" />
-            </td>
-            <td className="presscell radius1"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/mycointainer.png")} alt="" />
+              </td>
 
-            <td className="presscell radius2">
-              <img src={require("./news/bithumb.png")} alt="" />
-            </td>
-            <td className="presscell radius1">
-              <img src={require("./news/cryptopotato.png")} alt="" />
-            </td>
-            <td className="presscell radius5"></td>
-            <td className="presscell radius8"></td>
-            <td className="presscell radius1">
-              <img src={require("./news/coingape.png")} alt="" />
-            </td>
+              <td className="presscell radius1">
+                <img src={require("./news/blokonomi.png")} alt="" />
+              </td>
+              <td className="radius10 presscell"></td>
+              <td className="presscell radius1">
+                <img src={require("./news/techbullion.png")} alt="" />
+              </td>
 
-            <td className="presscell radius2"></td>
-          </tr>
-        </tbody>
-      </table>
-      <div className="rightbg"></div>
+              <td className="presscell radius6"></td>
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius4">
+                <img src={require("./news/utoday.png")} alt="" />
+              </td>
 
+              <td className="presscell radius7"></td>
+
+              <td className="presscell radius1">
+                <img src={require("./news/cryptopress.png")} alt="" />
+              </td>
+              <td className="radius7 presscell"></td>
+
+              <td className="presscell radius10">
+                <img src={require("./news/coinspeaker.png")} alt="" />
+              </td>
+              <td className="presscell radius1">
+                <img src={require("./news/cryptocompare.png")} alt="" />
+              </td>
+              <td className="presscell radius7">
+                <img src={require("./news/cointelegraph.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+
+              <td className="presscell radius7">
+                <img src={require("./news/mycointainer.png")} alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className="presscell radius7">
+                <img src={require("./news/uptrennd.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+
+              <td className="presscell radius6">
+                <img src={require("./news/coindex.png")} alt="" />
+              </td>
+
+              <td className="presscell radius1"></td>
+              <td className="presscell radius9">
+                <img src={require("./news/coindoo.png")} alt="" />
+              </td>
+
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius8">
+                <img src={require("./news/coinmanager.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+
+              <td className="presscell radius2">
+                <img src={require("./news/bithumb.png")} alt="" />
+              </td>
+              <td className="presscell radius1">
+                <img src={require("./news/cryptopotato.png")} alt="" />
+              </td>
+              <td className="presscell radius5"></td>
+              <td className="presscell radius8"></td>
+              <td className="presscell radius1">
+                <img src={require("./news/coingape.png")} alt="" />
+              </td>
+
+              <td className="presscell radius2"></td>
+              <td className="presscell radius7">
+                <img src={require("./news/uptrennd.png")} alt="" />
+              </td>
+              <td className="radius1 presscell"></td>
+
+              <td className="presscell radius6">
+                <img src={require("./news/coindex.png")} alt="" />
+              </td>
+
+              <td className="presscell radius1"></td>
+              <td className="presscell radius9">
+                <img src={require("./news/coindoo.png")} alt="" />
+              </td>
+
+              <td className="radius1 presscell"></td>
+              <td className="presscell radius8">
+                <img src={require("./news/coinmanager.png")} alt="" />
+              </td>
+              <td className="presscell radius1"></td>
+
+              <td className="presscell radius2">
+                <img src={require("./news/bithumb.png")} alt="" />
+              </td>
+              <td className="presscell radius1">
+                <img src={require("./news/cryptopotato.png")} alt="" />
+              </td>
+              <td className="presscell radius5"></td>
+              <td className="presscell radius8"></td>
+              <td className="presscell radius1">
+                <img src={require("./news/coingape.png")} alt="" />
+              </td>
+
+              <td className="presscell radius2"></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="rightbg"></div>
       </div>
     </div>
   );
