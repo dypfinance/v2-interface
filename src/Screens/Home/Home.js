3ecb2830730f65efaea86f60a3bbd1f6e4043df9
LorenaL579 <lorena.lici579@gmail.com>
2022-10-25 15:31:54 +0200

    test for dyptool

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -15,12 +15,12 @@ const Home = () => {
     <div className="homepage main-wrapper container-fluid pl-0 pr-0 d-flex flex-column align-items-center" style={{gap: '70px', paddingBottom: '5rem', overflowX: 'clip'}}>
       <MainHero/>
       <DypMetaverse/>
-      <DypTools/>
+      {/* <DypTools/> */}
       <DypToken/>
       <Suspense fallback={<div>Loading</div>}>
       <DypPartners />
       </Suspense>
-      {/* <DypNews topTitle='Announcements' bottomTitle='Stay tuned'/> */}
+      <DypNews topTitle='Announcements' bottomTitle='Stay tuned'/>
     </div>
   );
 };
