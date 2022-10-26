4f2d06bc69ee92b0c1166c284b36002415945164
LorenaL579 <lorena.lici579@gmail.com>
2022-10-26 13:44:59 +0200

    other test

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -14,17 +14,13 @@ const Home = () => {
   return (
     <div className="homepage main-wrapper container-fluid pl-0 pr-0 d-flex flex-column align-items-center" style={{gap: '70px', paddingBottom: '5rem', overflowX: 'clip'}}>
       <MainHero/>
-      <DypMetaverse/>
-      <DypTools/>
-      <DypMetaverse/>
-      <DypTools/>      <DypMetaverse/>
-      <DypTools/>      <DypMetaverse/>
-      <DypTools/>
-      {/* <DypToken/> */}
-      <Suspense fallback={<div>Loading</div>}>
-      {/* <DypPartners /> */}
-      </Suspense>
-      {/* <DypNews topTitle='Announcements' bottomTitle='Stay tuned'/> */}
+      {/* <DypMetaverse/>
+      <DypTools/> */}
+      <DypToken/>
+      {/* <Suspense fallback={<div>Loading</div>}> */}
+      <DypPartners />
+      {/* </Suspense> */}
+      <DypNews topTitle='Announcements' bottomTitle='Stay tuned'/>
     </div>
   );
 };
