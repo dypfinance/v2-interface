2ce52dc958ed78951737dcf5dc3b4a1d968c69f7
LorenaL579 <lorena.lici579@gmail.com>
2022-10-26 11:46:38 +0200

    removed metaverse for testing

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <div className="homepage main-wrapper container-fluid pl-0 pr-0 d-flex flex-column align-items-center" style={{gap: '70px', paddingBottom: '5rem', overflowX: 'clip'}}>
       <MainHero/>
-      <DypMetaverse/>
+      {/* <DypMetaverse/> */}
       <DypTools/>
       <DypToken/>
       <Suspense fallback={<div>Loading</div>}>
