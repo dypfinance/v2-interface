cbc1646ebc52b047154491b2ad067fc4e54c03f2
doyledyp <doyledev@protonmail.com>
2022-12-12 17:40:14 +0200

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./Styles/main.scss";
+import ReactGA from 'react-ga';
+
+const GOOGLE_ANALYTICS_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+if (typeof GOOGLE_ANALYTICS_ID === 'string') {
+    ReactGA.initialize(GOOGLE_ANALYTICS_ID)
+    // ReactGA.set({
+    //     customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
+    // })
+} else {
+    ReactGA.initialize('test', { testMode: true, debug: true })
+}
+
+window.addEventListener('error', error => {
+    ReactGA.exception({
+        description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
+        fatal: true
+    })
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
