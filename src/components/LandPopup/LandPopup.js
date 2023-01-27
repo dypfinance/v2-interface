b5f0ae4bafd87508dbdc230bce08cb74378fafbe
AldiAlinj <aldialinj0@gmail.com>
2023-01-27 12:19:33 +0100

    optimised land pop up

--- src/components/LandPopup/LandPopup.js
+++ src/components/LandPopup/LandPopup.js
@@ -41,8 +41,8 @@ const LandPopup = () => {
             <h6 className="popup-title mb-2">Land NFT Launch</h6>
             <span className="popup-span mb-0">Become one of the original land owners and gain access to immediate rewards and earn opportunities in the World of Dypians by minting a Genesis Land NFT</span>
         </div>
-        <img src={landPopup} className="w-75" alt="land nft" />
-        <span className="popup-content">Total Genesis land supply limited to only 1,000 parcels</span>
+        <img src={landPopup} className="land-nft-image" alt="land nft" />
+        <span className="popup-content">Total Genesis land supply limited to only 1,000 plots</span>
         <a href='https://www.worldofdypians.com/land' target={"_blank"} onClick={() => setActive(false)}>
         <button className="btn filled-btn m-3">Explore more</button>
         </a>
