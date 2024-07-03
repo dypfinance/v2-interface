7fe60fc3382b316c932c30187328714f1f943d0f
LorenaL579 <lorena.lici579@gmail.com>
2024-07-03 10:39:12 +0200

    added mobile flyout

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -7,24 +7,21 @@ import DypNews from "./DypNews/DypNews";
 import DypPartners from "./DypPartners/DypPartners";
 import "./DypNews/_dypnews.scss";
 import LandPopup from "../../components/LandPopup/LandPopup";
-import OutsideClickHandler from "react-outside-click-handler";
-import { isMobile, MobileView, BrowserView } from "react-device-detect";
-import xMark from "../../components/Navbar/assets/xMark.svg";
 
-const Home = () => {
+
+const Home = ({onDownloadClick}) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const [showPopup, setshowPopup] = useState(false);
 
   return (
-    <div className="homepage main-wrapper container-fluid pl-0 pr-0 d-flex flex-column align-items-center">
+    <div className="homepage main-wrapper container-fluid pl-0 pr-0 d-flex flex-column align-items-center mt-5 mt-lg-0">
       <MainHero />
       <DypMetaverse />
       <DypTools
         onDownloadClick={() => {
-          setshowPopup(true);
+         onDownloadClick()
         }}
       />
       <DypToken />
@@ -33,56 +30,7 @@ const Home = () => {
       </Suspense>
       <DypNews topTitle="Announcements" bottomTitle="Stay tuned" />
       <LandPopup />
-      <OutsideClickHandler onOutsideClick={() => setshowPopup(false)}>
-        <div
-          className={`metaverse-modal-wrapper2 ${
-            showPopup && "metaverse-active"
-          } flex-column p-4 d-flex gap-3 align-items-center justify-content-center`}
-        >
-          <img
-            src={xMark}
-            alt=""
-            className="x-mark"
-            onClick={() => setshowPopup(false)}
-          />
-          <h4 className="mobile-app-title">Dypius Mobile App</h4>
-          <div className="mobile-popup-wrapper p-3">
-            <ul className="mobile-content-list">
-              <li className="text-secondary">
-                Available exclusively in APK format for Android devices.
-              </li>
-              <li className="text-secondary">
-                Early release with some fully functional features.
-              </li>
-              <li className="text-secondary">
-                Other features are in view-only mode, relying on the MetaMask
-                Unity SDK.
-              </li>
-              <li className="text-secondary">
-                MetaMask-related issues are beyond our control; we're seeking
-                support to resolve them.
-              </li>
-              <li className="text-secondary">
-                Your feedback is valuable as we continue to improve the app.
-              </li>
-              <li className="text-secondary">
-                Thank you for your understanding and patience.
-              </li>
-            </ul>
-          </div>
-          <div className="separator2 w-100"></div>
-          <div className="d-flex justify-content-center">
-            <MobileView>
-              <a href="https://drive.google.com/file/d/1EvPyW0YWYcMc_x6sWViGYJxMStbg13D5/view" target="_blank" rel="noreferrer"  className={` d-flex align-items-center filled-btn btn `}>Download on mobile</a>
-            </MobileView>
-            <BrowserView>
-              <button className={`btn-secondary btn `} disabled={!isMobile}>
-                Download on mobile
-              </button>
-            </BrowserView>
-          </div>
-        </div>
-      </OutsideClickHandler>
+
     </div>
   );
 };
---
+++ src/components/MobileFlyout/MobileFlyout.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./_mobileflyout.scss";
+import newTag from "./assets/newTag.svg";
+import close from "./assets/close.svg";
+const MobileFlyout = ({ onClose, onDownloadClick }) => {
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const changeBackground = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY < 300) {
+        setShow(false);
+      } else {
+        if (window.scrollY > lastScrollY) {
+          setShow(true);
+        } else {
+          setShow(false);
+        }
+      }
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  const html = document.querySelector("html");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", changeBackground);
+      window.addEventListener("click", changeBackground);
+      window.addEventListener("load", changeBackground);
+    }
+
+   
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+      window.removeEventListener("click", changeBackground);
+      window.removeEventListener("load", changeBackground);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <div className={`mobile-flyout-wrapper p-2 w-100 d-block d-lg-none d-md-none  `}>
+      <div className="d-flex align-items-center gap-2">
+        <img src={newTag} alt="" />
+        <span className="mobile-flyout-text w-100">Dypius Mobile App</span>
+        <div className="d-flex w-100 justify-content-end">
+          <button
+            className="download-button px-3 py-1"
+            onClick={onDownloadClick}
+          >
+            Download
+          </button>
+        </div>
+        <div className="d-flex justify-content-end">
+          <img
+            src={close}
+            alt=""
+            onClick={onClose}
+            style={{ width: 22, height: 22 }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileFlyout;
